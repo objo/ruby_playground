9f132f438e8cca3281d903a6f6e3d8f1c217e0d8
Joe OBrien <joe@objo.com>
2017-10-30 17:45:14 -0400

    adding the rest of the notes for array and hash

--- ruby_101.rb
+++ ruby_101.rb
@@ -1,32 +1,115 @@
-#
-# Everything in ruby is an object
-#
-s = String.new("collection of characters")
+# Put out one line
+puts "Hello World"
+puts "to my friends"
 
-# Strings can be created with simple quotes
-s = "another string"
-s = 'yet another string'
+# Put out a line without a
+# return at the end
+print "Hello agiain "
+print "to the rest of my friends"
+puts " "
 
-# Fun things to do with Strings
+# Put a character
+putc "."
 
-string = "someone's name"
+puts " "
 
-string.reverse
+# variables
+my_string = "Simply create them with quotations"
 
-string.capitalize
+puts my_string
+
+# Strings
+one_string = 'one two three'
+another_string = "four five six"
+
+puts one_string
+puts another_string
+
+with_quote = ' "To be or not to be" - Shakespear'
+
+puts with_quote
+
+with_double_quote = " \"To be or not to be\" - Shakespear"
+
+puts with_double_quote
+
+# most common reason to use double quotes is for
+# interprelation
+
+puts "I like to eat #{2 + 1} times a day "
+
+puts 'I like to eat #{2 + 1} times a day '
 
 
-array = Array.new
+# getting information from someone
+# do that with `gets`
 
-song_one = "Wild Side"
-song_two = "Don't know what ya got"
-song_three = "Youth gone wild"
+puts "what is your name?"
 
-array = [1,3,2,4,5]
+name = gets.chomp
 
-array.each { |item| item * 2 }
+puts "I have your name as: " + name
 
-array.each do |item|
-  puts "value is"
-  puts item * 2
-end
+
+# Numbers
+
+one = 42
+two = 21
+
+one + two
+
+# Arrays
+
+# created with brackets
+colors = [ "Blue", "Green"]
+
+colors.pop
+
+# add using << operator
+colors << "Teal"
+
+# reference using open brackets and numbers (zero based!)
+
+puts colors[0]
+puts colors[1]
+puts colors[2]
+
+# Replacing items
+
+colors [1] = "Red"
+
+puts colors # should now print out ["Blue", "Red", "Green"]
+
+# methods by default return the new value
+
+colors << "Orange"
+colors << "Orange"
+colors << "Orange"
+
+new_colors = colors.uniq
+
+puts colors
+
+# Dangerous methods use a ! at the end
+
+colors.uniq! # this changes the original
+
+puts colors
+
+# Splitting a string into an array of characters
+
+name = "my name"
+
+puts name.split # will output ["my", "name"]
+
+# by default split will 'split' the string at on spaces
+
+puts name.split('') # will output ['m','y','n' .....]
+
+
+# Substitution in a string
+
+# sub substitutes the first instance
+puts "joe".sub('o', '0')  # outputs "j0e"
+
+# gsub substitutes ALL instances
